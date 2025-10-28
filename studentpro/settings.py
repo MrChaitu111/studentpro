68b17ed40624cc4d78cbf09793a267ffd86f2159
Mrchaitu111 <dcahitanya373@gmail.com>
2025-10-28 11:44:24 +0530

    Initial commit for Student_app Django project

--- studentpro/settings.py
+++ studentpro/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
+STATIC_DIR =os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES=[STATIC_DIR]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
